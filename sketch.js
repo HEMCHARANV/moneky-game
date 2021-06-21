ccd6eb294d7bb1942aea254c5cc0d48154361104
HEM CHARAN <hemcharan321@gmail.com>
2021-06-21 20:57:22 +0530

    done code

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var monkey, monkey_running, monkey_jumping
+var banana
+var obstacleImage, obstacleGroup
+var background
+
+var score
+
+function preload(){
+  
+  background=loadImage("jungle.png");
+  monkey_running=loadAnimation("monkey_01","monkey_02","monkey_03","monkey_04","monkey_05","monkey_06","monkey_07","monkey_08","monkey_09","monkey_10");
+               
+  
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);\
+  
+  background=createSprite
+}
+
+function draw() {
+  background(220);
+}
